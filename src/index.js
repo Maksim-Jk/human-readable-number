b8f1ab7debb20ad4978db124aad8cb5333942a1e
Maksim Zhuikov <sinplym1@gmail.com>
2023-08-04 09:21:25 +0500

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const words = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 0 || number >= 1000) {
+        return "Number out of range";
+    }
+
+    if (number < 20) {
+        return words[number];
+    }
+
+    if (number < 100) {
+        const tens_digit = Math.floor(number / 10);
+        const ones_digit = number % 10;
+        return (
+            words[20 + tens_digit] +
+            (ones_digit > 0 ? " " + words[ones_digit] : "")
+        );
+    }
+
+    const hundreds_digit = Math.floor(number / 100);
+    const remaining_number = number % 100;
+    return (
+        words[hundreds_digit] +
+        " hundred" +
+        (remaining_number > 0 ? " " + toReadable(remaining_number) : "")
+    );
+};
